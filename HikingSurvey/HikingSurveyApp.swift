469f145d64cde882ab87573e6ec2f4a21e45b452
Rob Breshears <robbreshears@gmail.com>
2025-11-05 13:52:47 -0700

    Initial Commit

---
+++ HikingSurvey/HikingSurveyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingSurveyApp.swift
+//  HikingSurvey
+//
+//  Created by Breshears, Rob - CTC on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct HikingSurveyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HikingSurveyTests/HikingSurveyTests.swift
@@ -0,0 +1,17 @@
+//
+//  HikingSurveyTests.swift
+//  HikingSurveyTests
+//
+//  Created by Breshears, Rob - CTC on 11/5/25.
+//
+
+import Testing
+@testable import HikingSurvey
+
+struct HikingSurveyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
