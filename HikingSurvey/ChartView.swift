7aa40c20396635a0657be54d8e1aca1ca6dd480c
Rob Breshears <robbreshears@gmail.com>
2025-11-05 18:04:15 -0700

    All complete

--- HikingSurvey/ChartView.swift
+++ HikingSurvey/ChartView.swift
@@ -18,7 +18,7 @@ struct ChartView: View {
     
     var body: some View {
         Chart(responses) { response in
-            SectorMark(angle: .value("Type", 1))
+            SectorMark(angle: .value("Type", 1), innerRadius: .ratio(0.75))
                 .foregroundStyle(by: .value("sentiment", response.sentiment))
         }
         .chartForegroundStyleScale([
@@ -28,6 +28,20 @@ struct ChartView: View {
             
             
         ])
+        .chartBackground { ChartProxy in
+            GeometryReader { geometry in
+                if let anchor = ChartProxy.plotFrame {
+                    
+                    let frame = geometry[anchor]
+                    Image(systemName: "figure.hiking")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: frame.height * 0.4)
+                        .foregroundStyle(Color(white: 0.59))
+                        .position(x: frame.midX, y: frame.midY)
+                }
+            }
+        }
         .chartLegend(position: .trailing, alignment: .center)
         .frame(height: 200)
         .padding()
